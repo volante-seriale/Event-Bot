b4490ed1059c816dd4187796365907809bb561bc
Volante <luiskopshti@outlook.com>
2026-05-05 17:28:29 +0200

    commit v1.1.3

--- bot.py
+++ bot.py
@@ -139,7 +139,7 @@ async def create_event(
     
     embed = discord.Embed(
         title=name,
-        description=f"**Date**: {date} | **Time**: {timestamp_display}",
+        description=f"@everyone\n**Date**: {date} | **Time**: {timestamp_display}",
         color=discord.Color.gold()
     )
     
